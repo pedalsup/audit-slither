8c38971da9b53c4183bc034c052340af6e9f3349
rupen-pedalsup <rupen.jarsaniya@pedalsup.com>
2024-02-20 17:54:52 +0530

    update: change html format of report

--- bin/slither-report.js
+++ bin/slither-report.js
@@ -5,6 +5,9 @@ const path = require("path");
 const puppeteer = require("puppeteer");
 const { spawn } = require("child_process");
 const { fetchContractSourceCode } = require("../src/fetch-contract");
+const showdown = require("showdown");
+
+const converter = new showdown.Converter();
 
 // Paths
 const jsonReportPath = path.join(process.cwd(), "slither.json");
@@ -19,6 +22,7 @@ let contractAddress;
 let projectName;
 let slitherProcess;
 let networkName;
+let github_uri;
 
 const arg = process.argv;
 for (let i = 0; i < arg.length; i++) {
@@ -28,14 +32,14 @@ for (let i = 0; i < arg.length; i++) {
     projectName = arg[i + 1];
   } else if (arg[i] === "-n" || arg[i] === "--network") {
     networkName = arg[i + 1];
+  } else if (arg[i] === "-g" || arg[i] === "--github_uri") {
+    github_uri = arg[i + 1];
   }
 }
 
 if (contractAddress && !networkName) {
   throw new Error("Please provider network name");
 }
-
-deleteFile(jsonReportPath);
 
 if (contractAddress) {
   fetchContractSourceCode(contractAddress, networkName)
@@ -53,7 +57,14 @@ if (contractAddress) {
       console.log(err);
     });
 } else {
-  slitherProcess = spawn("slither", [".", "--json", jsonReportPath]);
+  slitherProcess = spawn("slither", [
+    ".",
+    "--json",
+    jsonReportPath,
+    "--checklist",
+    "--markdown-root",
+    github_uri,
+  ]);
   main();
 }
 
@@ -106,7 +117,7 @@ async function generatePDF(inputHtmlPath, outputPdfPath) {
   deleteFile(jsonReportPath);
   deleteFile(contractFilePath);
 
-  console.log("Slither report generated successfully");
+  console.log("------Slither report generated successfully------");
 }
 
 // Delete file
@@ -118,26 +129,45 @@ function deleteFile(path) {
 
 // Generate HTML Report
 function generateHTMLReport(data) {
+  const lowImapactIssues = data.results.detectors.filter(
+    (item) => item.impact === "Low"
+  );
+  const mediumImapactIssues = data.results.detectors.filter(
+    (item) => item.impact === "Medium"
+  );
+  const highImapactIssues = data.results.detectors.filter(
+    (item) => item.impact === "High"
+  );
+  const imformationalImapactIssues = data.results.detectors.filter(
+    (item) => item.impact === "Informational"
+  );
+
   const htmlTemplate = `
       <html>
       <head>
         <title>Report</title>
       </head>
       <style>
-        * {
-          margin: 0;
-          padding: 0;
-        }
         body {
-          max-width: 100vw;
-          width: 100%;
+          
           overflow-x: hidden;
           font-size: 12px;
         }
+        .page-break-before {
+          page-break-before: always;
+        }
+        
+        .page-break-after {
+          page-break-after: always;
+        }
+        
+        .avoid-page-break-inside {
+          page-break-inside: avoid;
+        }
+
         table {
           border: 1px solid black;
           border-collapse: collapse;
-          max-width: 100vw;
           width: 100%;
           margin-top: 10px;
         }
@@ -147,21 +177,90 @@ function generateHTMLReport(data) {
           padding: 4px;
           word-break: break-all;
           vertical-align: super;
+          text-align: center;
         }
       </style>
       <body>
         <h1>${projectName ? projectName + " -" : ""} Slither Report</h1>
-  
-        <table id="result-table">
+
+        <div class="page-break-after">
+        <h2>Findings Summary</h2>
+<p>The following issues were identified during the audit:</p>
+
+        <table>
           <tr>
-            <th>Impact</th>
-            <th>Confidence</th>
-            <th>Location</th>
-            <th>Description</th>
-            <th>Check</th>
+            <th style="color: red;">High</th>
+            <th style="color: orange;">Medium</th>
+            <th style="color: blue;">Low</th>
+            <th style="color: gray;">Informational</th>
           </tr>
-          ${generateResultsHTML(data.results.detectors)}
+          <tr>
+            <td>${highImapactIssues.length}</td>
+            <td>${mediumImapactIssues.length}</td>
+            <td>${lowImapactIssues.length}</td>
+            <td>${imformationalImapactIssues.length}</td>
+          </tr>
         </table>
+        </div>
+        
+        <h2>Technical Details</h2>
+        ${
+          highImapactIssues.length > 0
+            ? `<div class="page-break-after">
+              <h3>High</h3>
+              <div>
+                ${generateResultsHTML(
+                  data.results.detectors.filter(
+                    (item) => item.impact === "High"
+                  )
+                )}
+              </div>
+            </div>`
+            : ``
+        }
+
+        ${
+          mediumImapactIssues.length > 0
+            ? `<div class="page-break-after">
+              <h3>Medium</h3>
+              <div>
+                ${generateResultsHTML(
+                  data.results.detectors.filter(
+                    (item) => item.impact === "Medium"
+                  )
+                )}
+              </div>
+            </div>`
+            : ``
+        }
+
+        ${
+          lowImapactIssues.length > 0
+            ? `<div class="page-break-after">
+              <h3>Low</h3>
+              <div>
+                ${generateResultsHTML(
+                  data.results.detectors.filter((item) => item.impact === "Low")
+                )}
+              </div>
+            </div>`
+            : ``
+        }
+
+        ${
+          imformationalImapactIssues.length > 0
+            ? `<div>
+              <h3>Informational</h3>
+              <div>
+                ${generateResultsHTML(
+                  data.results.detectors.filter(
+                    (item) => item.impact === "Informational"
+                  )
+                )}
+              </div>
+            </div>`
+            : ``
+        }
       </body>
       </html>
     `;
@@ -172,15 +271,32 @@ function generateHTMLReport(data) {
 // Generate HTML (Table Row) for results
 function generateResultsHTML(results) {
   let html = "";
-  for (const result of results) {
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+
     html += `
-        <tr class="result">
-          <td style="min-width: 70px;">${result.impact}</td>
-          <td style="min-width: 70px;">${result.confidence}</td>
-          <td style="min-width: 150px;">${result.first_markdown_element}</td>
-          <td>${result.description}</td>
-          <td style="min-width: 100px;">${result.check}</td>
-        </tr>
+          <div class="result">
+            <h2>PU-${i < 9 ? "0" + (i + 1) : i + 1} - ${result.impact}: ${
+      result.elements.length > 0
+        ? result.elements[0].type + result.elements[0].name
+        : "Unknown"
+    }</h2>
+            <div>${converter.makeHtml(result.markdown)}</div>
+            <div>
+              <p>Files affected:</p>
+              <ul class="list">${printElements(result.elements)}</ul>
+            </div>
+          </div>
+        `;
+  }
+  return html;
+}
+
+function printElements(elements) {
+  let html = "";
+  for (const element of elements) {
+    html += `
+          <li>${element.name}:${element.type} - ${element.source_mapping.filename_relative}</li>
       `;
   }
   return html;
