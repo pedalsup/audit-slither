402aaa71bc7962f43858a24282d2b2d656aecb94
rupen-pedalsup <rupen.jarsaniya@pedalsup.com>
2024-02-21 11:05:17 +0530

    :zap: improvement: create generateHTML seperate function and implement header and footer in pdf view

--- bin/slither-report.js
+++ bin/slither-report.js
@@ -5,9 +5,7 @@ const path = require("path");
 const puppeteer = require("puppeteer");
 const { spawn } = require("child_process");
 const { fetchContractSourceCode } = require("../src/fetch-contract");
-const showdown = require("showdown");
-
-const converter = new showdown.Converter();
+const generateHtml = require("../src/generateHtml");
 
 // Paths
 const jsonReportPath = path.join(process.cwd(), "slither.json");
@@ -22,7 +20,7 @@ let contractAddress;
 let projectName;
 let slitherProcess;
 let networkName;
-let github_uri;
+let githubUri;
 
 const arg = process.argv;
 for (let i = 0; i < arg.length; i++) {
@@ -33,7 +31,7 @@ for (let i = 0; i < arg.length; i++) {
   } else if (arg[i] === "-n" || arg[i] === "--network") {
     networkName = arg[i + 1];
   } else if (arg[i] === "-g" || arg[i] === "--github_uri") {
-    github_uri = arg[i + 1];
+    githubUri = arg[i + 1];
   }
 }
 
@@ -63,7 +61,7 @@ if (contractAddress) {
     jsonReportPath,
     "--checklist",
     "--markdown-root",
-    github_uri,
+    githubUri,
   ]);
   main();
 }
@@ -81,7 +79,7 @@ async function main() {
   slitherProcess.on("close", () => {
     const jsonData = require(jsonReportPath);
     if (jsonData.success) {
-      const html = generateHTMLReport(jsonData);
+      const html = generateHtml(jsonData, projectName, githubUri);
       fs.writeFileSync(htmlReportPath, html);
 
       (async () => {
@@ -108,7 +106,37 @@ async function generatePDF(inputHtmlPath, outputPdfPath) {
     path: outputPdfPath,
     format: "A4",
     printBackground: true,
-    margin: { top: 10, left: 10, right: 10, bottom: 10 },
+    margin: { top: 40, left: 40, right: 40, bottom: 40 },
+    displayHeaderFooter: true,
+    headerTemplate: `<header style='
+      width: 100%;
+      font-size: 10px;
+      color: #000;
+      padding-inline: 40px;
+      padding-bottom: 10px;
+      margin-bottom: 10px;
+      border-bottom: 1px solid #000;
+    '>
+      <div>
+        Pedalsup
+      </div>
+    </header>`,
+    footerTemplate: `<footer style=' 
+      width: 100%;
+      font-size: 10px;
+      color: #000;
+      padding-inline: 40px;
+      padding-top: 10px;
+      border-top: 1px solid #000;
+    '>
+      <div
+        style='
+          text-align: right;
+        '
+      >
+        <span class='pageNumber'></span> / <span class='totalPages'></span>  
+      </div>
+    </footer>`,
   });
 
   await browser.close();
@@ -125,171 +153,4 @@ function deleteFile(path) {
   if (fs.existsSync(path)) {
     fs.unlinkSync(path);
   }
-}
-
-// Generate HTML Report
-function generateHTMLReport(data) {
-  const lowImpactIssues = data.results.detectors.filter(
-    (item) => item.impact === "Low" && !item.first_markdown_element.toString().includes("node_modules")
-  );
-  const mediumImpactIssues = data.results.detectors.filter(
-    (item) => item.impact === "Medium" && !item.first_markdown_element.toString().includes("node_modules")
-  );
-  const highImpactIssues = data.results.detectors.filter(
-    (item) => item.impact === "High" && !item.first_markdown_element.toString().includes("node_modules")
-  );
-  const informationalImpactIssues = data.results.detectors.filter(
-    (item) => item.impact === "Informational" && !item.first_markdown_element.toString().includes("node_modules")
-  );
-
-  const htmlTemplate = `
-      <html>
-      <head>
-        <title>Report</title>
-      </head>
-      <style>
-        body {
-          
-          overflow-x: hidden;
-          font-size: 12px;
-        }
-        .page-break-before {
-          page-break-before: always;
-        }
-        
-        .page-break-after {
-          page-break-after: always;
-        }
-        
-        .avoid-page-break-inside {
-          page-break-inside: avoid;
-        }
-
-        table {
-          border: 1px solid black;
-          border-collapse: collapse;
-          width: 100%;
-          margin-top: 10px;
-        }
-        th,
-        td {
-          border: 1px solid black;
-          padding: 4px;
-          word-break: break-all;
-          vertical-align: super;
-          text-align: center;
-        }
-      </style>
-      <body>
-        <h1>${projectName ? projectName + " -" : ""} Slither Report</h1>
-
-        <div class="page-break-after">
-        <h2>Findings Summary</h2>
-<p>The following issues were identified during the audit:</p>
-
-        <table>
-          <tr>
-            <th style="color: red;">High</th>
-            <th style="color: orange;">Medium</th>
-            <th style="color: blue;">Low</th>
-            <th style="color: gray;">Informational</th>
-          </tr>
-          <tr>
-            <td>${highImpactIssues.length}</td>
-            <td>${mediumImpactIssues.length}</td>
-            <td>${lowImpactIssues.length}</td>
-            <td>${informationalImpactIssues.length}</td>
-          </tr>
-        </table>
-        </div>
-        
-        <h2>Technical Details</h2>
-        ${
-          highImpactIssues.length > 0
-            ? `<div class="page-break-after">
-              <h3>High</h3>
-              <div>
-                ${generateResultsHTML(highImpactIssues)}
-              </div>
-            </div>`
-            : ``
-        }
-
-        ${
-          mediumImpactIssues.length > 0
-            ? `<div class="page-break-after">
-              <h3>Medium</h3>
-              <div>
-                ${generateResultsHTML(
-                  mediumImpactIssues
-                )}
-              </div>
-            </div>`
-            : ``
-        }
-
-        ${
-          lowImpactIssues.length > 0
-            ? `<div class="page-break-after">
-              <h3>Low</h3>
-              <div>
-                ${generateResultsHTML(
-                  lowImpactIssues
-                  )}
-              </div>
-            </div>`
-            : ``
-        }
-
-        ${
-          informationalImpactIssues.length > 0
-            ? `<div>
-              <h3>Informational</h3>
-              <div>
-                ${generateResultsHTML(
-                  informationalImpactIssues
-                )}
-              </div>
-            </div>`
-            : ``
-        }
-      </body>
-      </html>
-    `;
-
-  return htmlTemplate;
-}
-
-// Generate HTML (Table Row) for results
-function generateResultsHTML(results) {
-  let html = "";
-  for (let i = 0; i < results.length; i++) {
-    const result = results[i];
-
-    html += `
-          <div class="result">
-            <h4>PU-${i < 9 ? "0" + (i + 1) : i + 1} - ${result.impact}: ${
-      result.elements.length > 0
-        ? result.elements[0].type + result.elements[0].name
-        : "Unknown"
-    }</h4>
-            <div>${github_uri ? converter.makeHtml(result.markdown): result.description}</div>
-            <div>
-              <p>Files affected:</p>
-              <ul class="list">${printElements(result.elements)}</ul>
-            </div>
-          </div>
-        `;
-  }
-  return html;
-}
-
-function printElements(elements) {
-  let html = "";
-  for (const element of elements) {
-    html += `
-          <li>${element.name}:${element.type} - ${element.source_mapping.filename_relative}</li>
-      `;
-  }
-  return html;
 }
